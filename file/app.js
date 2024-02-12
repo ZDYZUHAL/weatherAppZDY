223a9eefaa33a1c04ec5e89225f32aee20b76ab1
Zuhal <zuhal.daud1@gmail.com>
2024-02-12 04:42:31 -0800

    add api

--- file/app.js
+++ file/app.js
@@ -1,8 +1,23 @@
+function refresh(response) {
+  let numelement = document.querySelector("#temperature");
+  let temprecher = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  numelement.innerHTML = math.round(temperature);
+}
+
+function searchcity(city) {
+  let apikey = "00173a535d83oadtfd4476bbc54c7c2b";
+  let apiurl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}$units=metric";
+  axios.get(apiurl).then(refresh);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchcity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#searchform");
